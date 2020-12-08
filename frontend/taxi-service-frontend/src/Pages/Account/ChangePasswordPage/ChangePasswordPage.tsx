c495580bfa7905254644b5da1608560c5d4fb812
Szalkai Krisztián Farkas <szalkaititi@gmail.com>
2020-12-08 17:33:35 +0100

    component refactor

--- frontend/taxi-service-frontend/src/Pages/Account/ChangePasswordPage/ChangePasswordPage.tsx
+++ frontend/taxi-service-frontend/src/Pages/Account/ChangePasswordPage/ChangePasswordPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import AccountPageWrapper from '../Wrapper/AccountPageWrapper';
+import AccountPageWrapper from '../../../Components/AccountPageWrapper/AccountPageWrapper';
 import './ChangePasswordPage.scss';
 
 export default function ChangePasswordPage() {
     return(
         <AccountPageWrapper header="Change Password">
             <div>
-                
+
             </div>
         </AccountPageWrapper>
     )
