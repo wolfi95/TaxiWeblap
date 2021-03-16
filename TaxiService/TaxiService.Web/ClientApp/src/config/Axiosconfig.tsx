87d5ed0ecf5979e3fa9386e330b59712bd245ed1
Szalkai Krisztián Farkas <szalkaititi@gmail.com>
2021-03-16 21:56:52 +0100

    čonfig fix

--- TaxiService/TaxiService.Web/ClientApp/src/config/Axiosconfig.tsx
+++ TaxiService/TaxiService.Web/ClientApp/src/config/Axiosconfig.tsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-//const baseUrl = "https://taxi-service-diploma.herokuapp.com/api";
-const baseUrl = "https://localhost:5001/api";
+const baseUrl = "https://taxi-service-diploma.herokuapp.com/api";
+//const baseUrl = "https://localhost:5001/api";
 
 export const axiosInstance = Axios.create({
     baseURL: baseUrl
