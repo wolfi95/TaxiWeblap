a68ee768733deb43d4e1520ee9eeb6b7d1bd7795
Szalkai Krisztián Farkas <szalkaititi@gmail.com>
2021-04-13 16:18:07 +0200

    paymentId migration

---
+++ TaxiService/TaxiService.Dal/Migrations/20210413141737_PaymnetId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaxiService.Dal.Migrations
+{
+    public partial class PaymnetId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PaymentId",
+                table: "Reservations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "Reservations");
+        }
+    }
+}
